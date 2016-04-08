3b32a094e992d6df83593388d998024d0c791677
Mary Warrington <marywarrington@gmail.com>
2016-04-08 08:45:21 -0700

    Add bootstrap styling and nav bar

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   cuisine: DS.attr(),
-  address: DS.attr()
-  reviews: DS.hasMany('review', {async: true }),
+  address: DS.attr(),
+  reviews: DS.hasMany('review', { async: true })
 });

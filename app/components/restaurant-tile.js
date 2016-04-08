6ea8b469dc77dc3b8025a9c660e8c7cbfd70f18f
Mary Warrington <marywarrington@gmail.com>
2016-04-08 10:43:34 -0700

    Add fullInfo computed property

--- app/components/restaurant-tile.js
+++ app/components/restaurant-tile.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   favoriteRestaurants: Ember.inject.service(),
 
+  fullInfo: Ember.computed('restaurant.name', 'restaurant.cuisine', function() {
+    return this.get('restaurant.name') + ' -- ' + this.get('restaurant.cuisine');
+  }),
+
   actions: {
     addToFavorites(model) {
       this.get('restaurants').add(model);
